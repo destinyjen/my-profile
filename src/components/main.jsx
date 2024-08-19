c996d9a4d702051d669bc38ffe72f298b85756f4
Destiny <dejenkins@justworks.com>
2024-08-19 19:49:39 -0400

    project jsx component added

---
+++ src/components/main.jsx
@@ -0,0 +1,14 @@
+function Main() {
+    return (
+      <main>
+        <About />
+        <Project id="demo" projectNumber="1" />
+        <Project id="demo2" projectNumber="2" />
+      </main>
+    );
+  }
+  
+  export default Main;
+  // About.js
+  import React from 'react';
+  
---
+++ src/components/project.jsx
@@ -0,0 +1,12 @@
+  // Project.js
+  import React from 'react';
+  
+  function Project({ id, projectNumber }) {
+    return (
+      <section id={id}>
+        {/* Project Section Content */}
+      </section>
+    );
+  }
+  
+  export default Project;
