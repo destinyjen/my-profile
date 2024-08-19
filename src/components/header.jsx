ecdac9e273f5a5e656d483542774c34d0870d11b
Destiny <dejenkins@justworks.com>
2024-08-19 19:48:23 -0400

    header.jsx component addded for page

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+      <header>
+        <h2>Destiny's Portfolio</h2>
+        <nav>
+          {/* Navigation Links */}
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Header;
+  // Main.js
+  import React from 'react';
+  import About from './About';
+  import Project from './Project';
